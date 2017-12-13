d4ca70efb140fd23ffadf243555005388760129b
Kirill Volkovich <kirill.volkovich@jcatalog.com>
2017-12-13 14:57:05 +0300

    Update gh-pages

--- src/client/index.js
+++
@@ -1,6 +0,0 @@
-// Here should be es2016 or CommonJS exports.
-
-module.exports = {
-  SimpleAutocomplete: require('./components/SimpleAutocomplete').default,
-  VerticalList: require('./components/VerticalList').default
-};
