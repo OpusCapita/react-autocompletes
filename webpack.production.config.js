3d1e3287d8587a9c2452561fbaf4b61fff38e428
Egor Stambakio <31243790+estambakio-sc@users.noreply.github.com>
2018-05-31 14:41:26 +0300

    Removed dependencies from production bundle (#6)

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,16 +1,25 @@
 'use strict';
 const path = require('path');
 const webpack = require('webpack');
+const nodeExternals = require('webpack-node-externals');
+const config = require('./webpack.development.config.js');
 
-let config = require('./webpack.development.config.js');
 config.entry = path.resolve(path.join(__dirname, 'src', 'client', 'index.js'));
+
 delete config.devtool;
 delete config.output.publicPath;
 delete config.watch;
+
 config.plugins = config.plugins.concat([
   new webpack.DefinePlugin({
     'process.env.NODE_ENV': '"production"'
   })
 ]);
+
+config.externals = [
+  nodeExternals({
+    modulesFromFile: true
+  })
+];
 
 module.exports = config;
